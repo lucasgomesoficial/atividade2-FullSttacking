c4d589fdfe20a2af1304e7ff11d68a1e684e8703
Lucas Gomes <lucasgomesbr@gmail.com>
2024-04-16 05:47:11 -0300

    fixed web file

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -7,14 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
 };
+
 export default config;
